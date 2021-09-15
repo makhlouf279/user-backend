73844d436630c5568b2d52ff771f6994e9c50ce1
Makhlouf <makhloufaroua@gmail.com>
2021-09-14 21:37:55 -0400

    User-backend RestApi

--- src/main/java/com/makhlouf/user_backend/model/User.java
+++ src/main/java/com/makhlouf/user_backend/model/User.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//
     private  long id;
     private String  firstName;
     private String lastName;
